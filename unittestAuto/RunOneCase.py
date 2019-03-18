67f7375186217ec31091ddd9ef57f3ddbc98bc9f
bianwancheng <bwcheng01@163.com>
2019-03-18 22:17:36 +0800

    增加RunOneCase，pageMethod和Process分离

---
+++ unittestAuto/RunOneCase.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     RunOneCase.py
+   Description :
+   Author :       bianwancheng
+   date：          2019/3/18
+-------------------------------------------------
+   Change Activity:
+                   2019/3/18:
+-------------------------------------------------
+__author__ = 'wancheng.b'
+"""
+
+import os
+# 检查是否有测试案例
+import subprocess
+import threading
+import uiautomator2 as u2
+
+from unittestAuto.po.Process import Process
+
+# 检查设备连接
+from unittestAuto.public.PageMethod import getTest_info, existCase
+
+
+def getDevices():
+    '''
+    换行分割截取掉头和尾，然后用\T（Tab）截取
+    :return: devices_list
+    '''
+    devices = []
+    devicesName = subprocess.getoutput('adb devices')
+    devicesName = devicesName.split("\n")[1: -1]
+    for deviceName in devicesName:
+        deviceName = deviceName.split('\tdevice')
+        devices.append(deviceName[0])
+    return devices
+
+
+class MyThread(threading.Thread):
+    def __init__(self, device):
+        super(MyThread, self).__init__()  # 重写run()方法必须要写
+        self.device = device
+
+    '''
+    创建log，开启服务，加载用例
+    '''
+
+    def run(self):
+        # mkdirLog()
+        driver = u2.connect(self.device)
+        Process().runOneCase(driver, 'H:\homeGit\\unittest\\unittestAuto\yamls\Setting\settingXimalaya.yaml')
+
+
+if __name__ == '__main__':
+    if len(existCase(getTest_info('test_case', 'caseYaml'))) > 0:
+        for device in getDevices():
+            test_run = MyThread(device)
+            test_run.start()
+            test_run.join()
+
+    else:
+        print("测试案例不存在")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ unittestAuto/public/PageMethod.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     PageMethod
+   Description :
+   Author :       bianwancheng
+   date：          2019/3/18
+-------------------------------------------------
+   Change Activity:
+                   2019/3/18:
+-------------------------------------------------
+__author__ = 'wancheng.b'
+"""
+
+# 检查测试案例是否存在,return casedirlist
+import configparser
+import os
+
+import yaml
+
+'''
+测试案例是否存在return caseDirList
+'''
+
+
+def existCase(path):
+    caseList = []
+    for dirpath, dirname, files in os.walk(path):
+        for file in files:
+            # print(os.path.join(dirpath, file))
+            caseList.append(os.path.join(dirpath, file))
+    print('测试案例共有' + str(len(caseList)))
+    return caseList
+
+
+'''
+获取test_info.ini section下面的key对应的value值
+'''
+
+
+def getTest_info(section, key):
+    config = configparser.ConfigParser()
+    config.read('H:\homeGit\\unittest\\unittestAuto\data\\test_info.ini', encoding='utf-8')
+    return config.get(section, key)
+
+
+'''
+解析yaml，return：dict
+'''
+
+
+def getYaml(path):
+    with open(path, 'r', encoding='utf-8')as f:
+        deviceYaml = yaml.load(f)
+    return deviceYaml
+
+
+'''
+@ description 通过X, Y 坐标点击
+@:parameter driver, elementinfo_List
+
+'''
+
+
+def clickByXY(driver, elementinfo_List):
+    elementList = elementinfo_List.split(',')
+    return driver.click(float(elementList[0]), float(elementList[1]))
+
+'''
+@description 通过text点击
+@:parameter driver, elementinfo_List
+'''
+
+def clickByText(driver, elementinfo_List):
+    elementList = elementinfo_List.split(',')
+    return driver(text=elementList[0]).click(timeout=elementList[1])
